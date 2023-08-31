aa7920f45de86c85f9f05c96ecdada68a8f02780
Tyler Gillson <tyler.gillson@gmail.com>
2023-08-31 09:54:34 -0600

    fix: unique leader election id

--- cmd/main.go
+++ cmd/main.go
@@ -73,7 +73,7 @@ func main() {
 		Scheme:                 scheme,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "1f172fb1.spectrocloud.labs",
+		LeaderElectionID:       "2f172fb1.spectrocloud.labs",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
